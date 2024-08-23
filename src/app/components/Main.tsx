317d69576985c1fc8f6fc60b53ee669273cc55f7
yawarakacream <yawarakacream75@gmail.com>
2024-08-24 01:50:11 +0900

    feat: 都道府県の色をランダムに生成するように

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -2,6 +2,7 @@
 
 import { memo, useCallback, useEffect, useState } from "react";
 
+import { randInt } from "@/utility/math";
 import { DeepReadonly } from "@/utility/types";
 
 import { Prefecture } from "../actions/prefectures";
@@ -10,6 +11,7 @@ import PopulationGraph from "./PopulationGraph";
 import styles from "./Main.module.scss";
 
 export interface PrefectureForPlot extends Prefecture {
+  color: string;
   plot: boolean;
 }
 
@@ -23,7 +25,13 @@ export default memo(function Main({ prefectures: prefectures_ }: Props) {
   >([]);
 
   useEffect(() => {
-    setPrefectures(prefectures_.map((pref) => ({ ...pref, plot: false })));
+    setPrefectures(
+      prefectures_.map((pref) => ({
+        ...pref,
+        color: `hsl(${randInt(0, 360 + 1)}, ${randInt(0, 30 + 1) + 70}%, ${randInt(0, 20 + 1) + 50}%)`,
+        plot: false,
+      })),
+    );
   }, [prefectures_]);
 
   const onPrefectureButtonClick = useCallback((prefCode: number) => {
@@ -38,11 +46,12 @@ export default memo(function Main({ prefectures: prefectures_ }: Props) {
   return (
     <main className={styles["main"]}>
       <ul className={styles["prefecture-list"]}>
-        {prefectures.map(({ code, name, plot }) => (
+        {prefectures.map(({ code, name, color, plot }) => (
           <li key={code} className={styles["prefecture"]}>
             <PrefectureButton
               prefCode={code}
               prefName={name}
+              prefColor={color}
               checked={plot}
               onClick={onPrefectureButtonClick}
             />
