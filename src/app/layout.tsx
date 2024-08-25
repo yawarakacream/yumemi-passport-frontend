d10426177caba781fc8f47ed7aae39748739da8f
yawarakacream <yawarakacream75@gmail.com>
2024-08-26 02:01:22 +0900

    feat: ヘッダ・フッタを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 import "./globals.scss";
+import styles from "./layout.module.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "人口の推移",
-  description:
-    "ゆめみパスポート・フロントエンドコーディング試験、人口推移グラフの実装です。",
-};
+const title = "人口推移";
+const description =
+  "ゆめみパスポート・フロントエンドコーディング試験、人口推移グラフの実装です。";
+
+export const metadata: Metadata = { title, description };
 
 export default function RootLayout({
   children,
@@ -18,7 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className={styles["container"]}>
+          <header className={styles["header"]}>
+            <Link href="/">{title}</Link>
+          </header>
+          <div className={styles["main-wrapper"]}>{children}</div>
+          <footer className={styles["footer"]}>
+            &copy; 2024 yawarakacream
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
